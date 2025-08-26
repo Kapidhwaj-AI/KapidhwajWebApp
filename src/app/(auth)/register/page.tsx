43103c6358cbca994777453aa819b7c44d3bfec9
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-08-26 14:03:57 +0530

    register page changes

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,12 +1,11 @@
-// "use client";
+"use client";
 
 // import { RegisterFormController } from "@/controllers/auth/Register.form.controller";
 
-// export default function Register() {
-//   return (
+export default function Register() {
+  return (
 
+    <></>
 
-//     <RegisterFormController />
-
-//   );
-// }
+  );
+}
