b86b2cdd1d3f3fae924887ac1d8fb2bbf665bace
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-09-01 12:34:22 +0530

    base url changes and refresh token functionality added

--- src/lib/protectApi.ts
+++ src/lib/protectApi.ts
@@ -12,7 +12,6 @@ export interface ApiResponse<T> {
 export async function protectApi<T, D = undefined>(url: string,
     method?: Method,
     data?: D, type?: string, isNotCustomHeader?: boolean) {
-    const token = JSON.parse(getLocalStorageItem('kapi-token') ?? '{}')?.token
     const remoteHub = JSON.parse(getLocalStorageItem('Remotehub') ?? '{}')
     const localHub = JSON.parse(getLocalStorageItem('Remotehub') ?? '{}')
     const isValidHub = (remoteHub || localHub) && (typeof remoteHub === 'object' || typeof localHub === 'object') && ('id' in remoteHub || 'id' in localHub);
@@ -22,18 +21,53 @@ export async function protectApi<T, D = undefined>(url: string,
             : `http://${localHub.id}.local:8084`
         : apiBaseUrl;
     const headers: Record<string, string> = {
-        Authorization: `Bearer ${token}`,
         'Content-Type': type ?? 'application/json',
     };
     if (remoteHub && !isNotCustomHeader) {
         headers['x-hub-id'] = remoteHub.id;
     }
 
-    return axios<ApiResponse<T>>({
-        method: method ?? 'GET',
-        url: baseUrl + url,
-        data: data,
-        headers,
-    });
+    try {
+        const response = axios<ApiResponse<T>>({
+            method: method ?? 'GET',
+            url: baseUrl + url,
+            data: data,
+            headers,
+        });
+        return response
+    } catch (err) {
+        if (err?.response?.status === 401) {
+            console.log("Token expired, trying refresh...");
+
+            const refreshed = await fetchRefreshToken();
+            if (refreshed) {
+                return axios<ApiResponse<T>>({
+                    method: method ?? "GET",
+                    url: baseUrl + url,
+                    data: data,
+                    headers: headers,
+                });
+            }
+        }
+        console.error(err, "err from protectApi")
+        throw err;
+    }
+}
+
+
+export const fetchRefreshToken = async () => {
+
+    try {
+        const res = await axios.post(
+            `${apiBaseUrl}/refresh`,
+
+        );
+        console.log("Refresh API response:", res.status, res.data);
+        return true;
+
+    } catch (refreshErr: any) {
+        console.error("Refresh token failed:", refreshErr.message, refreshErr?.response?.data);
+        return false;
+    }
 }
 
