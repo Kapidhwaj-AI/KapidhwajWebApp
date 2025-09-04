d5fa1f5ed83602491ce05b704abc4a95659b33b7
Navin <navin@kapidhwaj.ai>
2025-09-04 10:55:09 +0530

    fix : token to access token

--- src/controllers/auth/Login.form.controller.tsx
+++ src/controllers/auth/Login.form.controller.tsx
@@ -61,9 +61,9 @@ export const LoginFormController = () => {
         const expiresAt = new Date();
         expiresAt.setDate(expiresAt.getDate() + 7);
 
-        setLocalStorageItem(LOCALSTORAGE_KEY, JSON.stringify({ token: res.data.token, expiresAt: expiresAt.toISOString() }))
+        setLocalStorageItem(LOCALSTORAGE_KEY, JSON.stringify({ token: res.data.access_token, expiresAt: expiresAt.toISOString() }))
         setLocalStorageItem('user', JSON.stringify(res.data.data))
-        dispatch(setAuthToken(res.data.token));
+        dispatch(setAuthToken(res.data.access_token));
 
         window.location.href = "/streams";
       }
