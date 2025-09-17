001386d5b208064eae6d84c6eb579489fdaaac43
mrskg0438 <gaudshwetkamal0438@gmail.com>
2025-09-17 18:49:30 +0530

    fix: components are overflowing

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -13,7 +13,7 @@ export default function AppLayout({
         <div className="flex flex-col md:flex-row gap-4 h-screen p-4">
             <SocketNotification />
             <Sidebar />
-            <main className="flex-1 relative bg-[var(--surface-200)] p-4 md:rounded-4xl rounded-2xl  flex flex-col gap-3 min-h-0">
+            <main className="flex-1 relative bg-[var(--surface-200)] p-4 md:rounded-4xl rounded-2xl flex flex-col gap-3 min-h-0 overflow-auto scrollbar-hide">
                 <TokenCheck />
                 <Breadcrumbs/>
                 {children}
